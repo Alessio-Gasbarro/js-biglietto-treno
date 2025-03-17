2720e6ea4ddbfacd2ccebe9f387c99a0d39b3089
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-03-17 15:12:29 +0100

    [UPD] Added Age prices

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+//Basic Variables
 let kmlenght = prompt("Quanti chilometri vuole percorrere Oggi?");
 let humanage = prompt("Inserisca l'età.");
 let kmprice = kmlenght * 0.21;
+let finalPrice = kmprice;
+
+//Age check
+if (humanage <= 18){
+    finalPrice = kmprice * 0.8;
+}
+
+if (humanage >= 65){
+    finalPrice = kmprice * 0.2;
+}
